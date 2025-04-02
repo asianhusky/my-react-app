6455d6aeecf923061ab940ca3779142c5acb82ad
Frank Jiang <fjiang@wustl.edu>
2025-04-01 19:31:54 -0500

    Checkpoint to review final code with the new implementation of Array and use states

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
-
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
-  
-}
-
-
-function handleClick(i) {
-  const nextSquares = squares.slice();
-  nextSquares[i] = "X";
-  setSquares(nextSquares);
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
-       <div className="board-row">
+      <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
