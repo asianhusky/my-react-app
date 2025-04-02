150ccd96ef8a125e4bf4e6af07ffe28d8b9db39f
Frank Jiang <fjiang@wustl.edu>
2025-04-01 19:40:46 -0500

    Before JumpTO method and still has console errors

--- src/App.jsx
+++ src/App.jsx
@@ -22,24 +22,6 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  function jumpTo(nextMove) {
-    // TODO
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -80,13 +62,31 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-         <ol>{moves}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
