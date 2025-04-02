d336c6ea18b40f768ced7024b94f1a3dc0c9304a
Frank Jiang <fjiang@wustl.edu>
2025-04-01 19:35:14 -0500

    function changes due to new implementation of nextSquares and slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({value, onSquareClick}) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -13,17 +13,17 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-
-    
-    const nextSquares = squares.slice();
-
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
+    if (squares[i]) {
+      return;
     }
 
-    nextSquares[i] = 'X';
+    const nextSquares = squares.slice();
+    
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
